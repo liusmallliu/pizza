04d211cc463b28ed01b3f1455cf84a63c7a32854
liusmallliu <lkliu1125@163.com>
2018-05-19 23:22:34 +0800

    pizza项目学习

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';       //这里一定要记得双引号，不然会报错
+export default {
+  components:{
+    //名字用驼峰规则,可以不用引号
+    appHeader:Header
+  }
+}
+</script>
+
+<style>
+
+</style>
